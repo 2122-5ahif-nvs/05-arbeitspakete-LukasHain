e23923e1ec0ff29ae5f69d2f70f282ac0fb0c3fc
LukasHain <lukashain9@gmail.com>
2021-11-26 13:34:32 +0100

    fix error in ChatSocket class

--- WebSocketsDemo/websockets-quickstart/src/main/java/websockets/at/htl/ChatSocket.java
+++ WebSocketsDemo/websockets-quickstart/src/main/java/websockets/at/htl/ChatSocket.java
@@ -1,14 +1,17 @@
 package websockets.at.htl;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.websocket.OnClose;
-import javax.websocket.OnMessage;
-import javax.websocket.OnOpen;
-import javax.websocket.Session;
-import javax.websocket.server.PathParam;
-import javax.websocket.server.ServerEndpoint;
+
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import javax.websocket.Session;
 
 @ServerEndpoint("/chat/{username}")
 @ApplicationScoped
@@ -17,19 +20,25 @@ public class ChatSocket {
     Map<String, Session> sessions = new ConcurrentHashMap<>();
 
     @OnOpen
-    public void onOpen(Session session, @PathParam("Username") String username) {
+    public void onOpen(Session session, @PathParam("username") String username) {
         sessions.put(username, session);
     }
 
     @OnClose
-    public void onClose(Session session, @PathParam("username") String username, Throwable throwable) {
+    public void onClose(Session session, @PathParam("username") String username) {
+        sessions.remove(username);
+        broadcast("User " + username + " left");
+    }
+
+    @OnError
+    public void onError(Session session, @PathParam("username") String username, Throwable throwable) {
         sessions.remove(username);
         broadcast("User " + username + " left on error: " + throwable);
     }
 
     @OnMessage
     public void onMessage(String message, @PathParam("username") String username) {
-        if (message.equalsIgnoreCase(("_ready_"))) {
+        if (message.equalsIgnoreCase("_ready_")) {
             broadcast("User " + username + " joined");
         } else {
             broadcast(">> " + username + ": " + message);
@@ -38,8 +47,10 @@ public class ChatSocket {
 
     private void broadcast(String message) {
         sessions.values().forEach(s -> {
-            s.getAsyncRemote().sendObject(message, result -> {
-                System.out.println("Unable to send message: " + result.getException());
+            s.getAsyncRemote().sendObject(message, result ->  {
+                if (result.getException() != null) {
+                    System.out.println("Unable to send message: " + result.getException());
+                }
             });
         });
     }
